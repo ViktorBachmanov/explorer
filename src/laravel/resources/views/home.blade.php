e3d572523d327d889aa9112f6d3ad1e396cf1b12
Viktor Bachmanov <vbachmanov@mail.ru>
2023-12-15 13:51:42 +0300

    Install TailwindCSS and configure it

--- src/laravel/resources/views/home.blade.php
+++ src/laravel/resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,7 +12,7 @@
 
         @vite(['resources/css/app.css', 'resources/vue/app.js'])
     </head>
-    <body>
+    <body class="dark:bg-black dark:text-white">
         <div id='app-vue'></div>
     </body>
 </html>
